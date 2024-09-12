d2c95c148a3d0e67b548c9f8167c7f77648af70f
jonas <j.t.meij@student.tudelft.nl>
2024-09-12 15:28:58 +0200

    model.py, finished the first two assignments

--- ex_2/model.py
+++ ex_2/model.py
@@ -51,6 +51,7 @@ class VanillaCNN(nn.Module):
 
     def __init__(self, channel_in, channel_out, last_layer_bias=True):
         super().__init__()
+<<<<<<< HEAD
         self.conv1 = nn.Conv2d(channel_in, 16, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
@@ -75,6 +76,30 @@ class VanillaCNN(nn.Module):
         x = F.relu(self.fc2(x))
 
         return self.fc3(x)
+=======
+
+        self.dummy_param = nn.Parameter(torch.empty(1))
+
+        self.conv1 = nn.Conv2d(channel_in, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=2, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=2, stride=1, padding=1)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.LazyLinear(128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, channel_out, bias=last_layer_bias)
+
+        pass
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv3(x)), 2)
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+>>>>>>> 1dcb7f0 (model.py, finished the first two assignments)
 
 
 def get_representation_layer(representation):
@@ -90,10 +115,19 @@ def get_representation_layer(representation):
                 super().__init__()
 
             def forward(self, x):
+<<<<<<< HEAD
                 I = 0.5 * x.sum(axis=1)
                 Q = x[:, 0] - x[:, 2]
                 U = x[:, 1] - x[:, 3]
                 return torch.stack((I, Q, U), axis=1)
+=======
+                self.I = 0.5 * (x[:, 0] + x[:, 1] + x[:, 2] + x[:, 3])
+                self.Q = x[:, 0] - x[:, 2]
+                self.U = x[:, 1] - x[:, 3]
+
+                return torch.stack([self.I, self.Q, self.U], dim=1)
+                # TODO: Implement the conversion from polarized intensity to IQU
+>>>>>>> 1dcb7f0 (model.py, finished the first two assignments)
 
         representation_layer = PolCh2IQU()
     elif representation == "DOP+AOP":
@@ -104,12 +138,21 @@ def get_representation_layer(representation):
                 super().__init__()
 
             def forward(self, x):
+<<<<<<< HEAD
                 I = 0.5 * x.sum(axis=1)
                 Q = x[:, 0] - x[:, 2]
                 U = x[:, 1] - x[:, 3]
                 DOP = torch.sqrt(Q**2 + U**2) / I
                 AOP = 0.5 * torch.arctan2(U, Q)
                 return torch.stack((DOP, AOP), axis=1)
+=======
+                self.I = 0.5 * (x[:, 0] + x[:, 1] + x[:, 2] + x[:, 3])
+                self.Q = x[:, 0] - x[:, 2]
+                self.U = x[:, 1] - x[:, 3]
+                self.DOP = torch.sqrt(self.Q**2 + self.U**2) / self.I
+                self.AOP = 0.5 * torch.atan2(self.U, self.Q)
+                return torch.stack([self.DOP, self.AOP], dim=1)
+>>>>>>> 1dcb7f0 (model.py, finished the first two assignments)
 
         representation_layer = PolCh2DOPAOP()
     elif representation == "IQU+DOP+AOP":
@@ -120,12 +163,22 @@ def get_representation_layer(representation):
                 super().__init__()
 
             def forward(self, x):
+<<<<<<< HEAD
                 I = 0.5 * x.sum(axis=1)
                 Q = x[:, 0] - x[:, 2]
                 U = x[:, 1] - x[:, 3]
                 DOP = torch.sqrt(Q**2 + U**2) / I
                 AOP = 0.5 * torch.arctan2(U, Q)
                 return torch.stack((I, Q, U, DOP, AOP), axis=1)
+=======
+                self.I = 0.5 * (x[:, 0] + x[:, 1] + x[:, 2] + x[:, 3])
+                self.Q = x[:, 0] - x[:, 2]
+                self.U = x[:, 1] - x[:, 3]
+                self.DOP = torch.sqrt(self.Q**2 + self.U**2) / self.I
+                self.AOP = 0.5 * torch.atan2(self.U, self.Q)
+
+                return torch.stack([self.I, self.Q, self.U, self.DOP, self.AOP], dim=1)
+>>>>>>> 1dcb7f0 (model.py, finished the first two assignments)
 
         representation_layer = PolCh2IQUDOPAOP()
 
